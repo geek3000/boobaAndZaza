60ef545d83d90a9629b00b6cddf6b403354db940
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-27 11:52:18 +0100

    WIP Mobile screen

---
+++ js/script.js
@@ -0,0 +1,9 @@
+nav = document.getElementById("nav");
+
+document.getElementById("menu").addEventListener('click', () => {
+    nav.classList.toggle("hide-mobile");
+});
+
+document.getElementById("exit").addEventListener('click', () => {
+    nav.classList.toggle("hide-mobile");
+});
