6e8b3ab7eac034e5749af3b2b58d45c3f7a1bfab
philippe SIMO <philippe.guemkamsimo@gmail.com>
2021-02-28 11:13:08 +0100

    mobile design OK

---
+++ js/carousel.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        margin: 5,
+        smartSpeed: 500,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          1000: {
+            items: 3 || 4,
+          },
+        },
+      });
+  });
+
+
