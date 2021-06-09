3169193f35e9d351a49669bc0f460d056078d7ad
ericmbouwe <ericmbouwe@yahoo.fr>
2021-06-09 13:48:07 +0100

    add mobile menu styles

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+const hamburgerBtn = document.getElementById('btnHamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+function openMenu(size) {
+  mobileMenu.style.width = `${size}%`;
+}
+
+function closeMenu() {
+  mobileMenu.style.width = '0';
+}
+
+hamburgerBtn.addEventListener('click', () => {
+  if (hamburgerBtn.classList.contains('open')) {
+    body.classList.remove('noscroll');
+    hamburgerBtn.classList.remove('open');
+    closeMenu();
+  } else {
+    body.classList.add('noscroll');
+    hamburgerBtn.classList.add('open');
+    openMenu(75);
+  }
+});
