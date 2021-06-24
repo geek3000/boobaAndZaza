a9fe5fdbe3217d0a26499da7c936259a5deb083f
ericmbouwe <ericmbouwe@yahoo.fr>
2021-06-24 23:45:55 +0100

    add donation carousel banner

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,44 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 15000,
-        autoplayHoverPause: true,
-        margin: 5,
-        smartSpeed: 500,
-        responsive: {
-          0: {
-            items: 1,
-          },
-          750: {
-            items: 2,
-          },
-          1350: {
-            autoplay: false,
-            loop: false,
-            items: 3 || 4,
-            margin:20
-          },
-        },
-      });
+$(document).ready(function () {
+  $('.owl-one').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 15000,
+    autoplayHoverPause: true,
+    margin: 5,
+    smartSpeed: 500,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      750: {
+        items: 2,
+      },
+      1350: {
+        autoplay: false,
+        loop: false,
+        items: 3 || 4,
+        margin: 20,
+      },
+    },
   });
+
+  $('.owl-two').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 15000,
+    autoplayHoverPause: true,
+    margin: 5,
+    smartSpeed: 500,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      750: {
+        items: 1,
+      },
+      1350: {
+        items: 1,
+      },
+    },
+  });
+});
